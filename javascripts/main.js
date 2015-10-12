2ce4dd7715f89bd4f537ae5b7c84b94df5d819bd
Kirk Graves <graves.kirk@gmail.com>
2015-10-12 18:36:35 -0500

    attempt to push to master

---
+++ javascripts/main.js
@@ -0,0 +1,64 @@
+requirejs.config({
+  baseUrl: './javascripts',
+  paths: {
+    'jquery': '../lib/bower_components/jquery/dist/jquery.min',
+    'lodash': '../lib/bower_components/lodash/lodash.min',
+    'hbs': '../lib/bower_components/require-handlebars-plugin/hbs',
+    'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min',
+    'q': '../lib/bower_components/q/q'
+  },
+  shim: {
+    'bootstrap': ['jquery']
+  }
+});
+
+
+
+require(["jquery", "createUser"], function($, createUser) {
+
+  $("#signupButton").click(function(e) {
+
+    // e.preventDefault();
+
+    console.log('Signup button clicked');
+
+    var newMember = {
+      "firstName": $("#firstName").val(),
+      "lastName": $("#lastName").val(),
+      "imgSrc": $("#imgSrc").val(),
+      "interests": $("#interests").val(),
+      "lookingFor": $("#lookingFor").val(),
+      "park": $("#park").val(),
+      "username": $("#username").val(),
+      "email": $("#email").val(),      
+      "password": $("#password").val()
+    };
+
+        
+    console.log('newMember', newMember);
+    
+    createUser.createMember(newMember);
+      
+  }) // end click event handler
+
+})
+
+require(["jquery", "profiles", "q", "hbs!../templates/memberProfiles"], function($, profiles, q, memTempl) {
+
+  // Change for git
+
+  // var newMembers;
+
+  profiles.loadProfiles()
+    .then(function(profileData) {
+      console.log('profileData', profileData);
+      // newMembers = profileData;
+      // console.log('newMembers', newMembers);
+    $('#profileDisplay').append(memTempl({newMembers: profileData})); 
+    })
+    
+})
+
+
+
+
